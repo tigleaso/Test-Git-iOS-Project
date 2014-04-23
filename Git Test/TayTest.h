9c98bbda5ee109685a5d5f091b165acc4e7b931c
Tim <tigleaso@yahoo.com>
2014-04-23 14:14:50 -0500

    added a test class

---
+++ Git Test/TayTest.h
@@ -0,0 +1,13 @@
+//
+//  TayTest.h
+//  Git Test
+//
+//  Created by Tim Gleason on 4/23/14.
+//  Copyright (c) 2014 Tim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TayTest : NSObject
+
+@end
---
+++ Git Test/TayTest.m
@@ -0,0 +1,13 @@
+//
+//  TayTest.m
+//  Git Test
+//
+//  Created by Tim Gleason on 4/23/14.
+//  Copyright (c) 2014 Tim. All rights reserved.
+//
+
+#import "TayTest.h"
+
+@implementation TayTest
+
+@end
