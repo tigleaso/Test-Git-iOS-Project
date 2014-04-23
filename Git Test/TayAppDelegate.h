1ab5dd007d28195939f48f8b9c5b3a95d758f30f
Tim <tigleaso@yahoo.com>
2014-04-23 14:06:47 -0500

    first commit

---
+++ Git Test/TayAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TayAppDelegate.h
+//  Git Test
+//
+//  Created by Tim Gleason on 4/23/14.
+//  Copyright (c) 2014 Tim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TayAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
